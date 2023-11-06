3c980be4e9c2cf46e9c252525115c46d9aa52bd5
msanlub898 <msanlub898@g.educaand.es>
2023-11-06 13:33:27 +0100

    Ejercicio1 diccionarios

---
+++ test/test_ejercicio1.py
@@ -0,0 +1,10 @@
+from src.ejercicio1 import dameSimboloDivisa
+import pytest
+
+def test_dameSimboloDivisa():
+    assert dameSimboloDivisa({'Euro':'€', 'Dollar':'$', 'Yen':'¥'},'Euro') == '€'
+
+
+def test_dameSimboloDivisaQueNoExiste():
+    assert dameSimboloDivisa({'Euro':'€', 'Dollar':'$', 'Yen':'¥'},'kk') == ''
+
