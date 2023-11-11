937181b388d160c5b9af6baa63b7847ff2d86da7
msanlub898 <msanlub898@g.educaand.es>
2023-11-11 13:47:35 +0100

    ok

--- test/test_ejercicio9.py
+++ test/test_ejercicio9.py
@@ -4,5 +4,3 @@ import pytest
 def test_añadirNuevaFactura():
     assert añadirNuevaFactura({},"4:40") == {"4":"40"}
 
-def test_procesoPagarFactura():
-    assert procesoPagarFactura({"4":"40"},"4") == "La factura se ha pagado."
